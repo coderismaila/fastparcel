db3b36583e5fa4e45de389ad18019154bedfa6c4
Ismaila Hassan <code.ismailah28@gmail.com>
2022-08-17 15:07:15 +0100

    refactor: courier and customer url templates

--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -1,22 +1,18 @@
-"""fastparcel URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
 from core import views
+from core.customer import views as customer_views
+from core.courier import views as courier_views
+
+customer_urlpatterns = [
+    path('', courier_views.home, name='home')
+]
+
+courier_urlpatterns = [
+    path('', customer_views.home, name='home')
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +23,6 @@ urlpatterns = [
     path('sign-out/', auth_views.LogoutView.as_view(next_page='/')),
     path('sign-up/', views.sign_up),
     
-    path('customer/', views.customer_page),
-    path('courier/', views.courier_page),
+    path('customer/', include((customer_urlpatterns, 'customer'))),
+    path('courier/', include((courier_urlpatterns, 'courier'))),
 ]
