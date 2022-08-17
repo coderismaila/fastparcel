386d82acad64cfb2d6c626b6ec4b385f3f24ddd3
Ismaila Hassan <code.ismailah28@gmail.com>
2022-08-17 10:01:51 +0100

    add sign in functionality

--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+
+    path('sign-in/', auth_views.LoginView.as_view(template_name='sign_in.html')),
+    path('sign-out/', auth_views.LogoutView.as_view(next_page='/')),
     path('customer/', views.customer_page),
     path('courier/', views.courier_page),
 ]
